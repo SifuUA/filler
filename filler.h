cae69bd226d6c2e6fc6a5d0fa2dcdcb745e02b82
SifuUA <alekseykres@gmail.com>
2017-03-20 10:43:22 +0200

    save

--- filler.h
+++ filler.h
@@ -2,4 +2,13 @@
 #define FILLER_FILLER_H_H
 
 # include "libft/libft.h"
+
+typedef struct s_fill
+{
+	char	**plateau;
+	char	**piece;
+	int 	player;
+	size_t	*size_m[2];
+	size_t 	*size_f[2];
+}				t_fill;
 #endif
--- main.c
+++ main.c
@@ -1,3 +1,27 @@
 #include "filler.h"
 
+static void 	write_in(t_fill *fill)
+{
+	while (get_next_line(0, fill->plateau))
+	{
+
+	}
+}
+
+t_fill			*get_mem(t_fill * fill)
+{
+
+}
+
+int 	main(void)
+{
+	t_fill *fill;
+
+	fill = malloc(sizeof(t_fill));
+	if (!fill)
+		exit(1);
+	fill->plateau = (char **)malloc(sizeof(char *) * 1000);
+	fill->piece = (char **)malloc(sizeof(char *) * 1000);
+	write_in(fill);
+}
 
